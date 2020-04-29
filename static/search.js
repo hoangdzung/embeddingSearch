33db4de6562b3a52aceb0d028b840604eadbce9b
hoangdzung <hoangdzung@github.com>
2020-04-29 16:06:37 +0700

    add style

--- static/search.js
+++ static/search.js
@@ -1,11 +1,16 @@
 $(function () {
     $('#search').click(function () {
+    	$('#rs_wrapper').hide();
+    	$('#loading').show();
         console.log("Press a button!")
         console.log(document.getElementById('query_type').value)
         console.log(document.getElementById('src_name').value)
         console.log(document.getElementById('target_type').value)
         $.ajax({
-            url: '/api/search?src_type=' + document.getElementById('query_type').value + '&src_name=' + document.getElementById('src_name').value + '&target_type=' + document.getElementById('target_type').value,
+            url: '/api/search?src_type=' + document.getElementById('query_type').value +
+             '&src_name=' + document.getElementById('src_name').value + 
+             '&target_type=' + document.getElementById('target_type').value + 
+             '&k=' + document.getElementById('num_return').value,
             success: function (data) {
                 console.log(data)
                 $('#result').html('');
@@ -13,6 +18,8 @@ $(function () {
                     console.log(data[i])
                     $('#result').append('<p>' + data[i]+ '</p>');
                 }
+                $('#rs_wrapper').show();
+                $('#loading').hide();
             }
         });
     });
