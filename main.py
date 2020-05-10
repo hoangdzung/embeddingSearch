50730292d327bc085db4942045f0f46f7607b2e3
QuangHuy <quanghuy27101997@gmail.com>
2020-05-10 15:01:26 +0700

    expand for manifold models

--- main.py
+++ main.py
@@ -7,12 +7,18 @@ import json
 app = Flask(__name__)  
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 app.config['TEMPLATES_AUTO_RELOAD'] = True
-query_machine = QueryMachine()
+query_machine = []#QueryMachine()
+queryMachines = []
 
 @app.route('/')  
 def upload():  
     # return render_template("file_upload_form.html")
-    return render_template("demo.html", meta_name = "Choose file",emb_name = "Choose file",types = ["Empty"],name_dict = [])  
+    dataFiles = [{
+        "id" : "model1",
+        "metaFile" : "Choose file",
+        "embFile" : "Choose file" 
+    }]
+    return render_template("demo.html", datasets = dataFiles ,types = ["Empty"],name_dict = [])  
 
 def clean(string):
     return string.replace('"','\\"')
@@ -25,28 +31,43 @@ def clean(string):
     # return to_save
 
 @app.route('/success', methods = ['POST'])  
-def success():  
-    if request.method == 'POST':  
-        meta_f = request.files['meta_file']
-        assert meta_f.filename.endswith('tsv')  
-        meta_f.save("temp_" + meta_f.filename)  
-        types, names = get_meta_from_tsv("temp_" + meta_f.filename)
+def success(): 
+    dataFiles = [] 
+    if request.method == 'POST':
+        numDatasets = int(request.form['numDatasets'])
+        global queryMachines
+        queryMachines = []
         name_dict = defaultdict(list)
-        for i in range(types.shape[0]):
-            name_dict[types[i]].append(clean(names[i]))
-            
-        emb_f = request.files['emb_file']  
-        assert emb_f.filename.endswith('tsv')
-        emb_f.save("temp_" + emb_f.filename)  
-        embeddings = get_embeddings_from_tsv("temp_" + emb_f.filename)
-
-        query_machine.update_data(embeddings, names, types)
+        for model in range(numDatasets):  
+            meta_f = request.files['meta_file_model' + str(model + 1)]
+            assert meta_f.filename.endswith('tsv')  
+            meta_f.save("temp_" + meta_f.filename)  
+            types, names = get_meta_from_tsv("temp_" + meta_f.filename)
+            for i in range(types.shape[0]):
+                name_dict[types[i]].append(clean(names[i]))
+                
+            emb_f = request.files['emb_file_model' + str(model + 1)]  
+            assert emb_f.filename.endswith('tsv')
+            emb_f.save("temp_" + emb_f.filename)  
+            embeddings = get_embeddings_from_tsv("temp_" + emb_f.filename)
+            query_machine = QueryMachine()
+            query_machine.update_data(embeddings, names, types)
+            queryMachines.append(query_machine)
+            print("qm",len(queryMachines))
+            dataFiles.append({
+                "id" : "model" + str(model + 1),
+                "metaFile" : meta_f.filename[:-4],
+                "embFile" : emb_f.filename[:-4]
+            })
         print(list(set(types)))
-        return render_template("demo.html", meta_name = meta_f.filename, emb_name = emb_f.filename, types = list(set(types)), name_dict = name_dict)  
+        return render_template("demo.html", datasets = dataFiles, types = list(set(types)), name_dict = name_dict)  
 
 @app.route("/api/search/<src_type>/<src_name>/<target_type>/<k>", methods = ["GET"])
 def search(src_type,src_name,target_type,k):
-    results = query_machine.search(src_type, src_name, target_type,int(k))
+    results = []
+    print("queryMachines",len(queryMachines))
+    for qMachine in queryMachines:
+        results.append(qMachine.search(src_type, src_name, target_type,int(k)))
     return jsonify(results)
 
 if __name__ == '__main__':  
--- static/search.js
+++ static/search.js
@@ -13,34 +13,94 @@ $(function () {
              '/' + document.getElementById('num-of-keywords').value,
             //  '&k=10' + document.getElementById('num_return').value,
             success: function (data) {
-                console.log(data)
-                $('#result').html('');
-                if(data.length == 0)
-                    $('#result').append('<h3 style="text-align: center;color: #999; margin-top: 20%;font-weight: 400;">No results</h3>');
-                else
-                    $('#result').append(`<div style="font-size: 0.95em; text-align: center;margin: 30px 0 20px 0;">
-                    NEAREST KEYWORDS
-                    </div>`);
+                console.log(data);
+                // resultModels = 
+                // $('#result').html('');
+                // if(data.length == 0)
+                //     $('#result').append('<h3 style="text-align: center;color: #999; margin-top: 20%;font-weight: 400;">No results</h3>');
+                // else
+                //     $('#result').append(`<div style="font-size: 0.95em; text-align: center;margin: 30px 0 20px 0;">
+                //     NEAREST KEYWORDS
+                //     </div>`);
                 for (i = 0; i < data.length; i++) {
                     console.log(data[i])
                     // $('#result-wrapper').append('<p>' + data[i]+ '</p>');
-                    $('#result').append(
-                        `
-                        <div class="item-keyword">
-                        <div style="padding: 5px 5px 0px 0">
-                            <img src="http://icons.iconarchive.com/icons/double-j-design/childish/128/Key-icon.png"
-                                width="18px"
-                                height="18px"
-                            >
+                    let idResult = '#result-model' + (i + 1) + " .content";
+                    console.log(idResult);
+                    $(idResult).html('');
+                    if(data[i].length == 0)
+                        $(idResult).append(
+                            `<h3 style="text-align: center;color: #999; margin: 30% 0;font-weight: 400;">No results</h3>`
+                        );
+                    for(j=0; j<data[i].length;j++){
+                        $(idResult).append(
+                            `
+                            <div class="item-keyword">
+                            <div style="padding: 5px 5px 0px 0">
+                                <img src="http://icons.iconarchive.com/icons/double-j-design/childish/128/Key-icon.png"
+                                    width="18px"
+                                    height="18px"
+                                >
+                            </div>
+                            <div style="padding: 4px;">` + data[i][j] + `</div>
                         </div>
-                        <div style="padding: 4px;">` + data[i] + `</div>
-                    </div>
-                    `
-                    );
+                        `
+                        );
+                    }
                 }
                 $('#result').show();
                 $('#loading').hide();
             }
         });
     });
-})
+});
+
+// function updateDataset() {
+//     let btnChooseFiles = document.getElementsByClassName("btn-choose-file");
+//     for(i=0;i<btnChooseFiles.length;i+=2){
+//         datasets[i/2] = {
+//             id: datasets[i/2].id,
+//             metaFile: btnChooseFiles[i+1].textContent,
+//             embFile: btnChooseFiles[i].textContent
+//         }
+//     }
+// } 
+
+function changeFile(e) {
+    var fileName = e.target.files[0].name;
+    $(this).parent().find('button').text(fileName);
+}
+
+
+function changeNumDatasets(){
+    var e = document.getElementById("num-of-datasets");
+    console.log(e.value);
+    const numDatasets = e.value;
+    document.getElementById("numDatasets").setAttribute("value",numDatasets);
+    $('#listModels').html('');
+    for(i=1;i<=numDatasets;i++) {
+        let idModel = 'model' + i;
+        $('#listModels').append(`
+        <div class="upload-model">
+            <h4 class="header">Model `+ i +`</h4>
+            <div class="content">
+                <div class="item-upload">
+                    <div>Vectors</div>
+                    <button type="button" class="btn-choose-file" onclick="document.getElementById('emb_file_`+idModel+`').click();">Choose file</button>
+                    <input type="file" name="emb_file_`+idModel+`" id="emb_file_`+idModel+`" style="display: none;" />
+                </div>
+                <div class="item-upload">
+                    <div>Metadata</div>
+                    <button type="button" class="btn-choose-file" onclick="document.getElementById('meta_file_`+idModel+`').click();">Choose file</button>
+                    <input type="file" name="meta_file_`+idModel+`" id="meta_file_`+idModel+`" style="display: none;" />
+                </div>
+            </div>
+        </div>
+        `);
+    }
+    $('input[type=file]').change(function (e) {
+        var fileName = e.target.files[0].name;
+        $(this).parent().find('button').text(fileName);
+        // updateDataset();
+    })
+}
